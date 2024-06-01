f1bbc000a1d9e736ea0ad523e57f935737022e01
Pasindu Upeka <upekasilva2000@gmail.com>
2024-06-01 10:02:01 +0530

    7

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,6 @@
+import DefaultCard from "./cards/DefaultCard.tsx";
+
 function Home(){
-
-    const maxWidthStyle:React.CSSProperties={
-
-        maxWidth:'540px'
-
-
-    }
 
 
     return (
@@ -18,86 +13,22 @@ function Home(){
                  <div className='row'>
 
                      <div className='col-12 col-sm-6 col-md-4 col-lg-3'>
-                         <div className="card mb-3" style={maxWidthStyle}>
-                             <div className="row g-0">
-                                 <div className="col-md-4">
-                                     <img src="..." className="img-fluid rounded-start" alt="..."/>
-                                 </div>
-                                 <div className="col-md-8">
-                                     <div className="card-body">
-                                         <h5 className="card-title">Card title</h5>
-                                         <p className="card-text">This is a wider card with supporting text below as a
-                                             natural lead-in to additional content. This content is a little bit
-                                             longer.</p>
-                                         <p className="card-text"><small className="text-body-secondary">Last updated 3
-                                             mins ago</small></p>
-                                     </div>
-                                 </div>
-                             </div>
-                         </div>
+
+                         <DefaultCard/>
+
+                     </div>
+                     <div className='col-12 col-sm-6 col-md-4 col-lg-3'>
+
+                         <DefaultCard/>
 
 
                      </div>
                      <div className='col-12 col-sm-6 col-md-4 col-lg-3'>
-                         <div className="card mb-3" style={maxWidthStyle}>
-                             <div className="row g-0">
-                                 <div className="col-md-4">
-                                     <img src="..." className="img-fluid rounded-start" alt="..."/>
-                                 </div>
-                                 <div className="col-md-8">
-                                     <div className="card-body">
-                                         <h5 className="card-title">Card title</h5>
-                                         <p className="card-text">This is a wider card with supporting text below as a
-                                             natural lead-in to additional content. This content is a little bit
-                                             longer.</p>
-                                         <p className="card-text"><small className="text-body-secondary">Last updated 3
-                                             mins ago</small></p>
-                                     </div>
-                                 </div>
-                             </div>
-                         </div>
-
+                         <DefaultCard/>
 
                      </div>
                      <div className='col-12 col-sm-6 col-md-4 col-lg-3'>
-                         <div className="card mb-3" style={maxWidthStyle}>
-                             <div className="row g-0">
-                                 <div className="col-md-4">
-                                     <img src="..." className="img-fluid rounded-start" alt="..."/>
-                                 </div>
-                                 <div className="col-md-8">
-                                     <div className="card-body">
-                                         <h5 className="card-title">Card title</h5>
-                                         <p className="card-text">This is a wider card with supporting text below as a
-                                             natural lead-in to additional content. This content is a little bit
-                                             longer.</p>
-                                         <p className="card-text"><small className="text-body-secondary">Last updated 3
-                                             mins ago</small></p>
-                                     </div>
-                                 </div>
-                             </div>
-                         </div>
-
-
-                     </div>
-                     <div className='col-12 col-sm-6 col-md-4 col-lg-3'>
-                         <div className="card mb-3" style={maxWidthStyle}>
-                             <div className="row g-0">
-                                 <div className="col-md-4">
-                                     <img src="..." className="img-fluid rounded-start" alt="..."/>
-                                 </div>
-                                 <div className="col-md-8">
-                                     <div className="card-body">
-                                         <h5 className="card-title">Card title</h5>
-                                         <p className="card-text">This is a wider card with supporting text below as a
-                                             natural lead-in to additional content. This content is a little bit
-                                             longer.</p>
-                                         <p className="card-text"><small className="text-body-secondary">Last updated 3
-                                             mins ago</small></p>
-                                     </div>
-                                 </div>
-                             </div>
-                         </div>
+                         <DefaultCard/>
 
 
                      </div>
