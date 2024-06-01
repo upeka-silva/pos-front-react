146e9ab0d3711804d7c59730ffd5f783349195bc
Pasindu Upeka <upekasilva2000@gmail.com>
2024-06-01 10:53:53 +0530

    8

--- src/components/cards/DefaultCard.tsx
+++ src/components/cards/DefaultCard.tsx
@@ -1,4 +1,15 @@
-function DefaultCard(){
+interface DefaultCardData{
+
+    thumbnail:string,
+    title:string,
+    description:string,
+    value:number
+
+}
+
+
+
+function DefaultCard(props:DefaultCardData){
 
 
     const maxWidthStyle:React.CSSProperties={
@@ -15,16 +26,13 @@ function DefaultCard(){
             <div className="card mb-3" style={maxWidthStyle}>
                 <div className="row g-0">
                     <div className="col-md-4">
-                        <img src="..." className="img-fluid rounded-start" alt="..."/>
+                        <img src={props.thumbnail} className="img-fluid rounded-start" alt={props.title}/>
                     </div>
                     <div className="col-md-8">
                         <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a
-                                natural lead-in to additional content. This content is a little bit
-                                longer.</p>
-                            <p className="card-text"><small className="text-body-secondary">Last updated 3
-                                mins ago</small></p>
+                            <h5 className="card-title">{props.title}</h5>
+                            <p className="card-text">{props.description}</p>
+                            <p className="card-text"><small className="text-body-secondary">{props.value}</small></p>
                         </div>
                     </div>
                 </div>
